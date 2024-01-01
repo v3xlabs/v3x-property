5fae642ce167bf033ae260995c636df844dfbd07
Luc <luc@lucemans.nl>
2024-01-01 22:40:37 +0000

    Introduce Axum

--- service/src/main.rs
+++ service/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use tracing::info;
+
+mod router;
+mod state;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    info!("Starting v3x-property");
+
+    let state = state::AppState::default();
+
+    router::serve(state).await.unwrap();
 }
