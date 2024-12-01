7c68ad75007ca5d6e346d3acca12547264935259
Luc <luc@lucemans.nl>
2024-12-02 00:19:09 +0100

    Update core http

--- web/src/api/core.ts
+++ web/src/api/core.ts
@@ -1,7 +1,6 @@
 import {
     DefinedUseQueryResult,
     QueryObserverOptions,
-    useMutation,
     useQuery,
 } from '@tanstack/react-query';
 
@@ -18,64 +17,53 @@ export type HttpOptions = {
     skipIfUnauthed?: boolean;
 };
 
+export const getHttp =
+    <T>(key: string, url: string, options?: HttpOptions) =>
+    async () => {
+        const { token, clearAuthToken } = useAuth.getState();
+        const { auth = 'ignore' } = options || {};
+
+        const headers = new Headers();
+
+        if (auth === 'include' || auth === 'required') {
+            if (!token && auth === 'required') {
+                throw new Error(
+                    'No token available but endpoint requires it, key: ' + key
+                );
+            }
+
+            headers.append('Authorization', 'Bearer ' + token);
+        }
+
+        try {
+            const response = await fetch(BASE_URL + key, { headers });
+
+            if (response.status === 401) {
+                console.log('Token expired, clearing token');
+                clearAuthToken();
+
+                throw new Error('Token expired');
+            }
+
+            return (await response.json()) as T;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    };
+
 export function useHttp<T>(
     key: string,
     options?: HttpOptions,
     queryOptions?: Partial<QueryObserverOptions>
 ): DefinedUseQueryResult<T, Error> {
-    const { token, clearAuthToken } = useAuth();
+    const { token } = useAuth();
     const { auth = 'ignore', skipIfUnauthed = false } = options || {};
 
     return useQuery({
         queryKey: [key],
         enabled: auth !== 'required' || !!token,
-        queryFn: async () => {
-            const headers = new Headers();
-
-            if (auth === 'include' || auth === 'required') {
-                if (!token && auth === 'required') {
-                    throw new Error(
-                        'No token available but endpoint requires it, key: ' +
-                            key
-                    );
-                }
-
-                headers.append('Authorization', 'Bearer ' + token);
-            }
-
-            try {
-                const response = await fetch(BASE_URL + key, { headers });
-
-                if (response.status === 401) {
-                    console.log('Token expired, clearing token');
-                    clearAuthToken();
-
-                    throw new Error('Token expired');
-                }
-
-                return (await response.json()) as T;
-            } catch (error) {
-                console.error(error);
-                throw error;
-            }
-        },
+        queryFn: getHttp(key, key, options),
         ...queryOptions,
-    });
-}
-
-// For mutations (if you have any POST/PUT/DELETE operations)
-export function useUpdateData<T>(url: string) {
-    const { token } = useAuth();
-
-    return useMutation({
-        mutationFn: (data: T) =>
-            fetch(BASE_URL + url, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${token}`,
-                },
-                body: JSON.stringify(data),
-            }).then((response) => response.json()),
-    });
+    }) as DefinedUseQueryResult<T, Error>;
 }
