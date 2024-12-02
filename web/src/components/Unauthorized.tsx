c615e4b0a50daa4682b3e86676234e9eed86164c
Luc <luc@lucemans.nl>
2024-12-02 14:58:25 +0100

    Introduce item deletion confirmation step

--- web/src/components/Unauthorized.tsx
+++ web/src/components/Unauthorized.tsx
@@ -1,9 +1,12 @@
+import { SCPage } from '../layouts/SimpleCenterPage';
+
 export const UnauthorizedResourceModal = () => {
     return (
-        <div>
-            <h1>Unauthorized</h1>
-            <p>You are not authorized to access this resource.</p>
-            <div>Try logging in</div>
-        </div>
+        <SCPage title="Unauthorized">
+            <div className="card">
+                <p>You are not authorized to access this resource.</p>
+                <div>Try logging in</div>
+            </div>
+        </SCPage>
     );
 };
--- web/src/routes/item/$itemId/edit.tsx
+++ web/src/routes/item/$itemId/edit.tsx
@@ -1,13 +1,62 @@
-import { createFileRoute, Link, useParams } from '@tanstack/react-router';
+import * as AlertDialog from '@radix-ui/react-alert-dialog';
+import {
+    createFileRoute,
+    Link,
+    useNavigate,
+    useParams,
+} from '@tanstack/react-router';
+import { FC } from 'react';
 
 import { useApiDeleteItem } from '../../../api/item';
 import { SCPage } from '../../../layouts/SimpleCenterPage';
 
+export const DeleteItemModal: FC<{ itemId: string }> = ({ itemId }) => {
+    const { mutate: deleteItem } = useApiDeleteItem();
+    const navigate = useNavigate();
+
+    return (
+        <AlertDialog.Root>
+            <AlertDialog.Trigger asChild>
+                <button className="btn btn-delete">Delete Item</button>
+            </AlertDialog.Trigger>
+            <AlertDialog.Portal>
+                <AlertDialog.Overlay className="bg-black/5 backdrop-blur-sm fixed inset-0" />
+                <AlertDialog.Content className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[90vw] max-w-[500px] bg-white focus:outline-none card space-y-2">
+                    <AlertDialog.Title className="m-0 text-blue-500 text-lg">
+                        Are you absolutely sure?
+                    </AlertDialog.Title>
+                    <AlertDialog.Description className="mb-6">
+                        This action cannot be undone. This will permanently
+                        delete this item.
+                    </AlertDialog.Description>
+                    <div className="gap-4 flex justify-end">
+                        <AlertDialog.Cancel asChild>
+                            <button className="btn">Cancel</button>
+                        </AlertDialog.Cancel>
+                        <AlertDialog.Action asChild>
+                            <button
+                                className="btn btn-delete"
+                                onClick={async () => {
+                                    await deleteItem(itemId);
+                                    navigate({
+                                        to: '/item/$itemId',
+                                        params: { itemId },
+                                    });
+                                }}
+                            >
+                                Yes, delete item
+                            </button>
+                        </AlertDialog.Action>
+                    </div>
+                </AlertDialog.Content>
+            </AlertDialog.Portal>
+        </AlertDialog.Root>
+    );
+};
+
 export const Route = createFileRoute('/item/$itemId/edit')({
     component: () => {
         const { itemId } = useParams({ from: '/item/$itemId/edit' });
-
-        const { mutate: deleteItem } = useApiDeleteItem();
 
         return (
             <SCPage title={`Edit Item ${itemId}`}>
@@ -20,14 +69,7 @@ export const Route = createFileRoute('/item/$itemId/edit')({
                     >
                         Save
                     </Link>
-                    <button
-                        className="btn btn-delete"
-                        onClick={() => {
-                            deleteItem(itemId);
-                        }}
-                    >
-                        Delete
-                    </button>
+                    <DeleteItemModal itemId={itemId} />
                 </div>
             </SCPage>
         );
