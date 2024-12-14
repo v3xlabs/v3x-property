036330a5062aa8983a81d835c019881720dc6271
Luc <luc@lucemans.nl>
2024-12-15 00:08:17 +0100

    Introduce barcode item fields

--- web/src/components/item/ItemFields.tsx
+++ web/src/components/item/ItemFields.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import Barcode from 'react-barcode';
+import Barcode, { BarcodeProps } from 'react-barcode';
 import { match } from 'ts-pattern';
 
 import { useItemFields } from '@/api/fields/item';
@@ -14,17 +14,32 @@ export const ItemFields: FC<{ item_id: string }> = ({ item_id }) => {
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 {fields?.map((field) =>
                     match(field)
-                        .with({ definition_id: 'ean' }, (field) => (
-                            <div>
-                                <div className="font-bold">
-                                    {field.definition_name}
+                        .with(
+                            { definition_id: 'ean' },
+                            { definition_id: 'upc' },
+                            { definition_id: 'gtin' },
+                            (field) => (
+                                <div>
+                                    <div className="font-bold">
+                                        {field.definition_name}
+                                    </div>
+                                    <div>{field.value as string}</div>
+                                    <div className="border flex justify-center items-center">
+                                        <Barcode
+                                            value={field.value as string}
+                                            format={
+                                                match(field.definition_id)
+                                                    .with('ean', () => 'EAN13')
+                                                    .with('upc', () => 'UPC')
+                                                    .otherwise(
+                                                        () => 'EAN13'
+                                                    ) as BarcodeProps['format']
+                                            }
+                                        />
+                                    </div>
                                 </div>
-                                <div>{field.value as string}</div>
-                                <div className="w-16">
-                                    <Barcode value={field.value as string} />
-                                </div>
-                            </div>
-                        ))
+                            )
+                        )
                         .otherwise(() => (
                             <div>
                                 <div className="font-bold">
