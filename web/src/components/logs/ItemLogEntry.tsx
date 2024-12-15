1919b155749d14965e54d81b90ab84457e60503f
Luc <luc@lucemans.nl>
2024-12-15 05:57:02 +0100

    Improve log experience

--- web/src/components/logs/ItemLogEntry.tsx
+++ web/src/components/logs/ItemLogEntry.tsx
@@ -20,6 +20,7 @@ export const ItemLogEntry = ({
     view?: 'global' | 'local';
 }) => {
     const created_ago = timeAgo.format(new Date(log.created_at));
+    const log_data = JSON.parse(log.data);
 
     return (
         <li className="block pt-2 first:pt-0">
@@ -32,30 +33,62 @@ export const ItemLogEntry = ({
                         />
                     )}
                 </div>
-                <div className="flex items-center gap-2">
+                <div className="flex flex-col w-full gap-2">
+                    <div className="flex justify-between items-start">
+                        <div className="flex items-center gap-2">
+                            {match({ action: log.action })
+                                .with({ action: 'create' }, () => (
+                                    <>
+                                        <span>Created by</span>
+                                        <UserProfile
+                                            user_id={log.user_id}
+                                            variant="compact"
+                                        />
+                                    </>
+                                ))
+                                .with({ action: 'edit' }, () => (
+                                    <>
+                                        <span>Edited by</span>
+                                        <UserProfile
+                                            user_id={log.user_id}
+                                            variant="compact"
+                                        />
+                                    </>
+                                ))
+                                .otherwise(() => log.action)}
+                        </div>
+                        <div className="text-sm text-gray-500">
+                            {created_ago}
+                        </div>
+                    </div>
                     {match({ action: log.action })
-                        .with({ action: 'create' }, () => (
-                            <>
-                                <span>Created by</span>
-                                <UserProfile
-                                    user_id={log.user_id}
-                                    variant="compact"
-                                />
-                            </>
-                        ))
                         .with({ action: 'edit' }, () => (
-                            <>
-                                <span>Edited by</span>
-                                <UserProfile
-                                    user_id={log.user_id}
-                                    variant="compact"
-                                />
-                            </>
+                            <div className="card no-padding p-2 w-full">
+                                <ul>
+                                    {Object.entries(log_data)
+                                        .filter(
+                                            ([_, b]) =>
+                                                (Array.isArray(b) &&
+                                                    b.length > 0) ||
+                                                (!Array.isArray(b) &&
+                                                    b != undefined)
+                                        )
+                                        .map(([key, value], index) => (
+                                            <li key={key} className="space-x-2">
+                                                <span className="font-bold">
+                                                    {key}
+                                                </span>
+                                                <span>{value as any}</span>
+                                            </li>
+                                        ))}
+                                </ul>
+                            </div>
                         ))
-                        .otherwise(() => log.action)}
+                        .otherwise(() => (
+                            <></>
+                        ))}
                 </div>
             </div>
-            <div className="text-sm text-gray-500">{created_ago}</div>
             {!['create', 'edit'].includes(log.action) && (
                 <div>
                     <UserProfile user_id={log.user_id} />
