18b3e33bf9ba9d6a6d03fdd422be9443b69beaf7
Luc <luc@lucemans.nl>
2024-12-15 01:18:18 +0100

    Introduce ASIN Field support

--- web/src/components/item/ItemFields.tsx
+++ web/src/components/item/ItemFields.tsx
@@ -1,30 +1,84 @@
-import { FC } from 'react';
+import { Link } from '@tanstack/react-router';
+import { FC, useState } from 'react';
 import Barcode, { BarcodeProps } from 'react-barcode';
+import { FaAmazon } from 'react-icons/fa6';
 import { match } from 'ts-pattern';
 
 import { useItemFields } from '@/api/fields/item';
 
-const PREFERRED_FIELD_ORDER = [''];
+import { FieldOption, FieldSelect } from '../form/Select';
+import { Button } from '../ui/Button';
+
+const AMAZON_DOMAINS = [
+    { label: 'Amazon.com', value: 'amazon.com', icon: () => <FaAmazon /> },
+    { label: 'Amazon Sweden', value: 'amazon.se', icon: () => <div>🇸🇪</div> },
+    {
+        label: 'Amazon Netherlands',
+        value: 'amazon.nl',
+        icon: () => <div>🇳🇱</div>,
+    },
+    { label: 'Amazon Germany', value: 'amazon.de', icon: () => <div>🇩🇪</div> },
+    { label: 'Amazon UK', value: 'amazon.co.uk', icon: () => <div>🇬🇧</div> },
+    { label: 'Amazon France', value: 'amazon.fr', icon: () => <div>🇫🇷</div> },
+    { label: 'Amazon Italy', value: 'amazon.it', icon: () => <div>🇮🇹</div> },
+    { label: 'Amazon Spain', value: 'amazon.es', icon: () => <div>🇪🇸</div> },
+] as FieldOption[];
 
 export const ItemFields: FC<{ item_id: string }> = ({ item_id }) => {
     const { data: fields } = useItemFields(item_id);
+    const [selectedDomain, setSelectedDomain] = useState(
+        AMAZON_DOMAINS[0].value
+    );
 
     return (
         <div className="p-4">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 {fields?.map((field) =>
                     match(field)
+                        .with({ definition_id: 'asin' }, (field) => (
+                            <div className="space-y-2">
+                                <div className="font-bold">
+                                    {field.definition_name}
+                                </div>
+                                <div className="card no-padding px-4 py-2">
+                                    {field.value as string}
+                                </div>
+                                <div className="flex items-center gap-2 justify-between">
+                                    <Button variant="default" asChild>
+                                        <Link
+                                            to={`https://${selectedDomain}/dp/${field.value}`}
+                                            target="_blank"
+                                            className="w-full"
+                                        >
+                                            View on Amazon
+                                            {'.' +
+                                                selectedDomain
+                                                    .split('.')
+                                                    .slice(1)
+                                                    .join('.')}
+                                        </Link>
+                                    </Button>
+                                    {/* TODO: Migrate to use Select component & fix height */}
+                                    <FieldSelect
+                                        value={selectedDomain}
+                                        onChange={(event) =>
+                                            setSelectedDomain(event)
+                                        }
+                                        options={AMAZON_DOMAINS}
+                                    />
+                                </div>
+                            </div>
+                        ))
                         .with(
                             { definition_id: 'ean' },
                             { definition_id: 'upc' },
                             { definition_id: 'gtin' },
                             (field) => (
-                                <div>
+                                <div className="space-y-2">
                                     <div className="font-bold">
                                         {field.definition_name}
                                     </div>
-                                    <div>{field.value as string}</div>
-                                    <div className="border flex justify-center items-center">
+                                    <div className="card no-padding flex items-center justify-center p-0.5">
                                         <Barcode
                                             value={field.value as string}
                                             format={
@@ -36,6 +90,9 @@ export const ItemFields: FC<{ item_id: string }> = ({ item_id }) => {
                                                     ) as BarcodeProps['format']
                                             }
                                         />
+                                    </div>
+                                    <div className="card no-padding px-4 py-2">
+                                        {field.value as string}
                                     </div>
                                 </div>
                             )
