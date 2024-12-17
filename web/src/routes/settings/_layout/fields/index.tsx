f0e8fe221bf0928174dfd705e35164244f73915f
Luc <luc@lucemans.nl>
2024-12-17 15:44:02 +0100

    Introduce partial field defs table

--- web/src/routes/settings/_layout/fields/index.tsx
+++ web/src/routes/settings/_layout/fields/index.tsx
@@ -1,9 +1,16 @@
 import { useForm } from '@tanstack/react-form';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router';
+import {
+    createColumnHelper,
+    flexRender,
+    getCoreRowModel,
+    useReactTable,
+} from '@tanstack/react-table';
 import { useState } from 'react';
-import { FaPencil, FaTrash } from 'react-icons/fa6';
+import { FaTrash } from 'react-icons/fa6';
 import { toast } from 'sonner';
+import { match } from 'ts-pattern';
 
 import {
     FieldDefinition,
@@ -19,7 +26,7 @@ import { Button } from '@/components/ui/Button';
 import * as Dialog from '@/components/ui/Dialog';
 import { queryClient } from '@/util/query';
 
-const FieldDefinitionEditor = ({
+const NewFieldDefinitionEditor = ({
     fieldDef,
     onSuccess,
 }: {
@@ -64,7 +71,7 @@ const FieldDefinitionEditor = ({
                 event.stopPropagation();
                 form.handleSubmit();
             }}
-            className="space-y-2"
+            className="space-y-2 flex"
         >
             <form.Field
                 name="definition_id"
@@ -226,6 +233,184 @@ const FieldDefinitionEditor = ({
     );
 };
 
+const FieldDefinitionEditor = ({
+    fieldDef,
+    onSuccess,
+}: {
+    fieldDef?: FieldDefinition;
+    onSuccess?: () => void;
+}) => {
+    const createMutation = useCreateFieldDefinition();
+    const updateMutation = useEditFieldDefinition();
+
+    const [previewIcon, setPreviewIcon] = useState<string | undefined>(
+        fieldDef?.icon
+    );
+
+    const form = useForm<FieldDefinition>({
+        defaultValues: fieldDef ?? {
+            definition_id: '',
+            kind: 'String', // Default kind
+            name: '',
+            description: '',
+            placeholder: '',
+            icon: '',
+        },
+        onSubmit: async ({ value }) => {
+            try {
+                await (fieldDef?.definition_id
+                    ? updateMutation.mutateAsync({
+                          field_id: fieldDef.definition_id,
+                          data: value,
+                      })
+                    : createMutation.mutateAsync(value));
+                onSuccess?.();
+            } catch (error) {
+                console.error('Failed to save field definition:', error);
+            }
+        },
+    });
+
+    return (
+        <form
+            onSubmit={(event) => {
+                event.preventDefault();
+                event.stopPropagation();
+                form.handleSubmit();
+            }}
+            className="space-y-2 flex"
+        >
+            <form.Field
+                name="name"
+                validators={{
+                    onChange: ({ value }) => {
+                        if (!value) {
+                            return 'Name is required';
+                        }
+                    },
+                }}
+            >
+                {(field) => (
+                    <BaseInput
+                        label="Name"
+                        name="name"
+                        value={field.state.value}
+                        onChange={field.handleChange}
+                        onBlur={field.handleBlur}
+                        width="full"
+                        errorMessage={field.state.meta.errors.join(', ')}
+                    />
+                )}
+            </form.Field>
+
+            <form.Field name="description">
+                {(field) => (
+                    <BaseInput
+                        label="Description"
+                        name="description"
+                        value={field.state.value}
+                        onChange={field.handleChange}
+                        onBlur={field.handleBlur}
+                        width="full"
+                        errorMessage={field.state.meta.errors.join(', ')}
+                    />
+                )}
+            </form.Field>
+
+            <form.Field name="placeholder">
+                {(field) => (
+                    <BaseInput
+                        label="Placeholder"
+                        name="placeholder"
+                        value={field.state.value}
+                        onChange={field.handleChange}
+                        onBlur={field.handleBlur}
+                        width="full"
+                        errorMessage={field.state.meta.errors.join(', ')}
+                    />
+                )}
+            </form.Field>
+
+            <form.Field
+                name="kind"
+                validators={{
+                    onChange: ({ value }) => {
+                        if (!value) {
+                            return 'Kind is required';
+                        }
+
+                        if (
+                            !['String', 'Number', 'Boolean', 'Json'].includes(
+                                value
+                            )
+                        ) {
+                            return 'Invalid kind';
+                        }
+                    },
+                }}
+            >
+                {(field) => (
+                    <div className="flex flex-col gap-2 w-full">
+                        <FieldSelect
+                            label="Kind"
+                            value={field.state.value}
+                            onChange={(value) =>
+                                field.handleChange(
+                                    value as
+                                        | 'String'
+                                        | 'Number'
+                                        | 'Boolean'
+                                        | 'Json'
+                                )
+                            }
+                            options={[
+                                { label: 'String', value: 'String' },
+                                { label: 'Number', value: 'Number' },
+                                { label: 'Boolean', value: 'Boolean' },
+                                { label: 'Json', value: 'Json' },
+                            ]}
+                        />
+                    </div>
+                )}
+            </form.Field>
+
+            <form.Field
+                name="icon"
+                validators={{
+                    onChange: ({ value }) => {
+                        setPreviewIcon(value);
+
+                        // eslint-disable-next-line unicorn/no-useless-undefined
+                        return undefined;
+                    },
+                }}
+            >
+                {(field) => (
+                    <IconInput
+                        value={field.state.value || ''}
+                        onChange={field.handleChange}
+                        errorMessage={field.state.meta.errors.join(', ')}
+                    />
+                )}
+            </form.Field>
+
+            <form.Subscribe
+                selector={(state) => [state.canSubmit, state.isSubmitting]}
+                children={([canSubmit, isSubmitting]) => (
+                    <div className="flex gap-2 items-center justify-end">
+                        <Button
+                            type="submit"
+                            disabled={!canSubmit || isSubmitting}
+                        >
+                            {isSubmitting ? 'Saving...' : 'Save'}
+                        </Button>
+                    </div>
+                )}
+            />
+        </form>
+    );
+};
+
 const RouteComponent = () => {
     const { data } = useSuspenseQuery(getFieldDefinitions());
 
@@ -234,6 +419,23 @@ const RouteComponent = () => {
     >();
 
     const [creatingField, setCreatingField] = useState<boolean>(false);
+
+    // const fields = [{ definition_id: 'hello' }];
+
+    const columnHelper = createColumnHelper<FieldDefinition>();
+
+    const table = useReactTable({
+        columns: [
+            columnHelper.accessor('icon', {
+                header: 'Icon',
+            }),
+            columnHelper.accessor('definition_id', {
+                header: 'Definition ID',
+            }),
+        ],
+        data,
+        getCoreRowModel: getCoreRowModel(),
+    });
 
     return (
         // suffix={
@@ -256,6 +458,110 @@ const RouteComponent = () => {
         // }
         <>
             <div className="card">
+                <table>
+                    <thead>
+                        {table.getHeaderGroups().map((header) => (
+                            <tr key={header.id}>
+                                {header.headers.map((header) => (
+                                    <th key={header.id}>
+                                        {header.isPlaceholder
+                                            ? undefined
+                                            : flexRender(
+                                                  header.column.columnDef
+                                                      .header,
+                                                  header.getContext()
+                                              )}
+                                    </th>
+                                ))}
+                            </tr>
+                        ))}
+                    </thead>
+                    <tbody>
+                        {table.getRowModel().rows.map((row) => (
+                            <tr key={row.id}>
+                                {row.getVisibleCells().map((cell) => {
+                                    // const item =
+                                    //     cell.getValue() as any as FieldDefinition[keyof FieldDefinition];
+                                    // const field_id =
+                                    //     cell.row.getValue('field_id');
+
+                                    return (
+                                        <td key={cell.id}>
+                                            {match({
+                                                type: cell.column.columnDef[
+                                                    'accessorKey'
+                                                ],
+                                            })
+                                                .with({ type: 'icon' }, () => (
+                                                    <IconInput
+                                                        value={
+                                                            cell.getValue()[
+                                                                'icon'
+                                                            ] ?? ''
+                                                            // field.state.value || ''
+                                                        }
+                                                        // onChange={
+                                                        //     field.handleChange
+                                                        // }
+                                                        // errorMessage={field.state.meta.errors.join(
+                                                        //     ', '
+                                                        // )}
+                                                    />
+                                                ))
+                                                .with(
+                                                    { type: 'definition_id' },
+                                                    ({ type }) => (
+                                                        // <form.Field
+                                                        //     name="definition_id"
+                                                        //     validators={{
+                                                        //         onChange: ({
+                                                        //             value,
+                                                        //         }) => {
+                                                        //             if (!value) {
+                                                        //                 return 'Definition ID is required';
+                                                        //             }
+                                                        //         },
+                                                        //     }}
+                                                        // >
+                                                        //     {(field) => (
+                                                        <BaseInput
+                                                            name="definition_id"
+                                                            value={
+                                                                cell.getValue()
+                                                                // field.state.value
+                                                            }
+                                                            // onChange={
+                                                            //     field.handleChange
+                                                            // }
+                                                            // onBlur={
+                                                            //     field.handleBlur
+                                                            // }
+                                                            width="full"
+                                                            // readOnly={!!fieldDef}
+                                                            // errorMessage={field.state.meta.errors.join(
+                                                            //     ', '
+                                                            // )}
+                                                        />
+                                                        // )}
+                                                        // </form.Field>
+                                                    )
+                                                )
+                                                .otherwise(() => (
+                                                    <>
+                                                        {flexRender(
+                                                            cell.column
+                                                                .columnDef.cell,
+                                                            cell.getContext()
+                                                        )}
+                                                    </>
+                                                ))}
+                                        </td>
+                                    );
+                                })}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
                 {data.map((field) => (
                     <div
                         className="flex items-center gap-2 rounded-md p-2 hover:bg-gray-50"
@@ -268,13 +574,12 @@ const RouteComponent = () => {
                             />
                         )}
                         {field.name}
-                        <Button
-                            onClick={() => setEditingField(field)}
-                            variant="ghost"
-                            className="ml-auto"
-                        >
-                            <FaPencil />
-                        </Button>
+                        <div>
+                            <FieldDefinitionEditor
+                                fieldDef={field}
+                                // eslint-disable-next-line unicorn/no-useless-undefined
+                            />
+                        </div>
                         <Button
                             variant="destructive"
                             onClick={() => {
@@ -287,22 +592,16 @@ const RouteComponent = () => {
                     </div>
                 ))}
             </div>
-
-            <Dialog.Root
-                open={!!editingField}
-                onOpenChange={(open) => {
-                    if (!open) {
-                        // eslint-disable-next-line unicorn/no-useless-undefined
-                        setEditingField(undefined);
-                    }
-                }}
-            >
+            <Dialog.Root open={creatingField} onOpenChange={setCreatingField}>
+                <Dialog.Trigger>
+                    <Button>Create Field Definition</Button>
+                </Dialog.Trigger>
                 <Dialog.Content>
-                    <Dialog.Title>Edit Field Definition</Dialog.Title>
-                    <FieldDefinitionEditor
-                        fieldDef={editingField!}
-                        // eslint-disable-next-line unicorn/no-useless-undefined
-                        onSuccess={() => setEditingField(undefined)}
+                    <Dialog.Title>Create Field Definition</Dialog.Title>
+                    <NewFieldDefinitionEditor
+                        onSuccess={() => {
+                            setCreatingField(false);
+                        }}
                     />
                 </Dialog.Content>
             </Dialog.Root>
