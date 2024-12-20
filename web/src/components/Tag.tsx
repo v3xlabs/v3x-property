3b953f2e9a36b9cc2157e6c5c555a51476640b34
Luc <luc@lucemans.nl>
2024-12-20 21:04:24 +0100

    Update tag colors

--- web/src/components/Tag.tsx
+++ web/src/components/Tag.tsx
@@ -15,6 +15,16 @@ const colorIsDark = (color: string) => {
     return (r * 0.299 + g * 0.587 + b * 0.114) < 186;
 };
 
+// text color generator (generate white or black-ish), make this color inspired by the color of the tag
+const textColorGenerator = (color: string) => {
+    if (!color) return 'rgba(0, 0, 0, 0.8)';
+
+    const isDark = colorIsDark(color);
+    const [r, g, b] = color.match(/\w\w/g)!.map(x => Number.parseInt(x, 16));
+
+    return isDark ? 'rgba(255, 255, 255, 0.9)' : `rgba(${r * 0.3}, ${g * 0.3}, ${b * 0.3}, 0.95)`;
+};
+
 export const Tag: FC<{ tag?: TagT, tag_id?: number, variant?: 'compact' | 'normal' }> = ({ tag, tag_id, variant = 'normal' }) => {
     const { data: tagDynamicData } = useTagById(tag_id);
     const tagData = tag ?? tagDynamicData;
@@ -26,9 +36,8 @@ export const Tag: FC<{ tag?: TagT, tag_id?: number, variant?: 'compact' | 'norma
             'text-nowrap rounded-md',
             variant === 'compact' ? 'px-1 py-0.5 text-xs' : 'px-2 py-1',
             tagData?.color ? '' : 'bg-gray-100 hover:bg-gray-200',
-            colorIsDark(tagData?.color ?? '') ? 'text-white' : 'text-black'
         )}
-        style={{ backgroundColor: tagData?.color }}
+        style={{ backgroundColor: tagData?.color, color: textColorGenerator(tagData?.color ?? '') }}
     >
         {tagData?.name}
     </Link>;
